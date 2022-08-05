a2521c3d47e8dd783dffd273b1e09c20a14ce777
Duy Tran <kp18899@gmail.com>
2022-08-05 21:24:47 +0700

    Golang study Section 3
    Variable Declerations

---
+++ section3/main.go
@@ -0,0 +1,13 @@
+package main
+
+import "fmt"
+
+var item1 string = "Here is the string of item1."
+
+func main() {
+	item2 := "Here is the string of item2."
+
+	fmt.Println(item1)
+	fmt.Println(item2)
+
+}
