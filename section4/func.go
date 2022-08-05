2294795e261b05ab7fbd3b040e08d5e7ab5af99a
Duy Tran <kp18899@gmail.com>
2022-08-06 00:58:33 +0700

    Golang study Section 4
    function & return type

---
+++ section4/func.go
@@ -0,0 +1,13 @@
+package main
+
+import "fmt"
+
+func call_state() string {
+	return "Running"
+}
+
+func number() {
+	fmt.Println(1)
+	fmt.Println(2)
+	fmt.Println(3)
+}
---
+++ section4/main.go
@@ -0,0 +1,14 @@
+package main
+
+import "fmt"
+
+var start string = "Staring..."
+
+func main() {
+
+	fmt.Println(start)
+	fmt.Println(call_state())
+
+	number()
+
+}
