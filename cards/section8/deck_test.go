c43ab56b381d8732b96e75cfff4992034a343587
Duy Tran <kp18899@gmail.com>
2022-08-22 21:14:28 +0700

    Golang study Section 10
    random number generation
    testing with Go
    testing file IO

--- cards/section8/deck_test.go
+++ cards/section8/deck_test.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"os"
 	"strings"
 	"testing"
 )
@@ -18,12 +19,27 @@ func TestNewDeck(t *testing.T) {
 
 	fmt.Println(strings.Contains(first_d, "Ace"))
 
-	if ! strings.Contains(first_d, "Ace") {
+	if !strings.Contains(first_d, "Ace") {
 		t.Errorf("First element of Slice is Wrong!")
 	}
 
 	fmt.Println(strings.Contains(last_d, "Four"))
-	if ! strings.Contains(last_d, "Four") {
+	if !strings.Contains(last_d, "Four") {
 		t.Errorf("Last element of Slice is Wrong!")
 	}
+}
+
+func TestSavetoDeckAndNewDeckFromFile(t *testing.T) {
+	os.Remove("_decktesting")
+
+	deck := newDeck()
+	deck.saveToFile("_decktesting")
+
+	deckload := newDeckFromFile("_decktesting")
+
+	if len(deckload) != 16 {
+		t.Errorf("Expected deck length of 16, but got %v", len(deckload))
+	}
+
+	os.Remove("_decktesting")
 }
