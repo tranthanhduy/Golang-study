e7570a39e0e11308fbe298558c9b38447ff5f5c9
Duy Tran <kp18899@gmail.com>
2022-10-12 18:31:31 +0700

    Golang study
    - Structs
    - Pointer

--- structs/main.go
+++ structs/main.go
@@ -2,19 +2,48 @@ package main
 
 import "fmt"
 
+type contactInfo struct {
+	email   string
+	zipCode int
+}
+
 type person struct {
 	firstName string
 	lastName  string
 	age       int
+	contact   contactInfo
 }
 
 func main() {
-	var duy person
 
-	duy.firstName = "Tran"
-	duy.lastName = "Duy"
-	duy.age = 24
+	duy := person{
+		firstName: "Tran",
+		lastName:  "Duy",
+		age:       18,
+		contact: contactInfo{
+			email:   "hehehihi@gmail.com",
+			zipCode: 700000,
+		},
+	}
 
-	fmt.Println(duy)
-	fmt.Printf("%+v", duy)
+	fmt.Println("Name")
+	duy.print()
+
+	// fmt.Println("\nNew name 1:")
+	// duyPointer := &duy
+	// duyPointer.updateName("Dinivity1")
+	// duy.print()
+
+	// NOTE: Go will automatically turn normal person type to pointer person type. This is called shortcut.
+	fmt.Println("\nNew name 2:")
+	duy.updateName("Dinivity")
+	duy.print()
+}
+
+func (pointerToPerson *person) updateName(newFirstName string) {
+	(*pointerToPerson).firstName = newFirstName
+}
+
+func (p person) print() {
+	fmt.Printf("%+v", p)
 }
