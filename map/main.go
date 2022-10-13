877e44a61fe8734c2880c0061ec2419e69f0c064
Duy Tran <kp18899@gmail.com>
2022-10-13 22:17:09 +0700

    Golang study
    Map

--- map/main.go
+++ map/main.go
@@ -3,19 +3,27 @@ package main
 import "fmt"
 
 func main() {
-	// colors := map[string]string{
-	// 	"red":   "#ff0000",
-	// 	"green": "#h123321",
-	// 	"blue":  "#ba12345",
-	// }
+	colors := map[string]string{
+		"red":   "#ff0000",
+		"green": "#h123321",
+		"blue":  "#ba12345",
+	}
 
-	colors := make(map[int]string)
+	// colors := make(map[int]string)
 
-	colors[123] = "yellow"
+	// colors[123] = "yellow"
 
-	fmt.Println(colors)
+	// fmt.Println(colors)
 
 	// Delete colors
-	delete(colors, 123)
-	fmt.Println(colors)
+	// delete(colors, 123)
+	// fmt.Println(colors)
+
+	printMap(colors)
+}
+
+func printMap(c map[string]string) {
+	for color, hex := range c {
+		fmt.Println("Hex code for", color, "is", hex)
+	}
 }
