50863efa1ca2a7d5739a0f1cdfbfbd2d474e8e6f
Duy Tran <kp18899@gmail.com>
2022-10-13 21:54:09 +0700

    Golang study
    - map

---
+++ map/main.go
@@ -0,0 +1,21 @@
+package main
+
+import "fmt"
+
+func main() {
+	// colors := map[string]string{
+	// 	"red":   "#ff0000",
+	// 	"green": "#h123321",
+	// 	"blue":  "#ba12345",
+	// }
+
+	colors := make(map[int]string)
+
+	colors[123] = "yellow"
+
+	fmt.Println(colors)
+
+	// Delete colors
+	delete(colors, 123)
+	fmt.Println(colors)
+}
